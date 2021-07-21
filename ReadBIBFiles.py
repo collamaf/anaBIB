9a64029ce87665d081eda177ce90f69250109138
collamaf <francesco.collamati@lnf.infn.it>
2021-07-21 12:55:29 +0200

    Add command line argument to pass the file path

--- ReadBIBFiles.py
+++ ReadBIBFiles.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[8]:
 
 
 import glob
 import argparse
 parser = argparse.ArgumentParser(description='Read data path')
-parser.add_argument('--dataPath', type=str, help='path')
-#path = "FLUKAresults/3TeV/prova3tev*
-args = parser.parse_args()
+parser.add_argument('--dataPath', type=str, help='input file path')
+parser.add_argument('--outPath', type=str, help='out file path')
 
-#path = "FLUKAresults/1.5TeV/*"
+args = parser.parse_args()
 if args.dataPath:
     path=args.dataPath
 else:
     path = "FLUKAresults/1.5TeV/*"
+    #path = "FLUKAresults/3TeV/prova3tev*
+    
+if args.outPath:
+    outfile=args.outPath
+else:
+    outfile = "DigFiles/OutFile"
+    #path = "FLUKAresults/3TeV/prova3tev*
+#outfile="DigFiles/"
 
 
 # In[3]:
@@ -42,7 +49,7 @@ print("Eventi TOT=", numMuonsTot)
 # In[4]:
 
 
-with open("OUT1p5","w") as fOut:
+with open(outfile,"w") as fOut:
     for filename in sorted(glob.glob(path)):
         with open(filename, 'r') as f:
             print("Aperto File", filename)
@@ -56,16 +63,4 @@ with open("OUT1p5","w") as fOut:
                                    .format(int(ele[0]), float(ele[1]), float(ele[2]), float(ele[3]), float(ele[4]), float(ele[5])*2e12/numMuonsTot, float(ele[6]), 
                                            float(ele[7]), float(ele[8]), float(ele[9]), ele[10], float(ele[11]), float(ele[12]), 
                                            float(ele[13]), int(ele[14]), ele[15], int(ele[16]) ))
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
