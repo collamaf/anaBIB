3cd3dd50d9ac1d687d5282af96f3b0e2872b8c71
CamillaCuratolo <87425466+CamillaCuratolo@users.noreply.github.com>
2021-11-01 14:27:28 +0100

    Update ReadBIBFiles.py

--- ReadBIBFiles.py
+++ ReadBIBFiles.py
@@ -1,12 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import glob
 import argparse
 import sys
+import numpy as np
 
 def is_interactive():
     import __main__ as main
@@ -20,24 +15,18 @@ parser.add_argument('--outPath', type=str, help='out file path')
 
 if is_interactive():
     sys.argv = ['-f']
-    
+
 args = parser.parse_args()
 if args.dataPath:
     path=args.dataPath
 else:
-    #path = "FLUKAresults/1.5TeV/*"
-    path = "FLUKAresults/1.5TeVcorr/*"
-    #path = "FLUKAresults/3TeV/prova3tev*
-    
+    path = "FLUKAresults/provanew" #/*"
+
 if args.outPath:
     outfile=args.outPath
 else:
-    outfile = "DigFiles/OutFile"
-    #path = "FLUKAresults/3TeV/prova3tev*
-#outfile="DigFiles/"
-
-
-# In[4]:
+    outfile1 = "FLUKAresults/3TeV_point_BH_WM_NEW"
+    outfile2 = "FLUKAresults/3TeV_point_BH_WM_NEW_electrons"
 
 
 numMuonsTot=0
@@ -46,8 +35,6 @@ for filename in sorted(glob.glob(path)):
         print("Aperto File", filename)
         for line in f:
             if  line.strip():
-#        line=f.readline()
-#            print(line[0]+"\n")
                 if line[1] == '#':
                     if line[4:8] == 'numb':
                         numMuons=int(line.split()[4])
@@ -57,37 +44,26 @@ for filename in sorted(glob.glob(path)):
                         continue
 print("Eventi TOT=", numMuonsTot)
 
-
-# In[21]:
-
-
-with open(outfile,"w") as fOut:
+count=0
+with open(outfile1,"w") as fOut1, open(outfile2,"w") as fOut2:
     for filename in sorted(glob.glob(path)):
         with open(filename, 'r') as f:
             print("Aperto File", filename)
             for line in f:
-#                print(line)
+                if  line[8:12] == 'muon' and count<1:
+                    nmd=line.split()
+                    count=2
                 if  line.strip():
-#                    print("CCCCCC",line[0:2])
-#                    if line[0] == '#':
-                    if '#' in line[0:2]:
-                        continue
-                    else:
+                    if line[1:5] == '## f':
                         ele=line.split()
-                        fOut.write("{:d}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{}\t{:e}\t{:e}\t{:e}\t{:d}\t{}\t{:d}\n"
-                                   .format(int(ele[0]), float(ele[1]), float(ele[2]), float(ele[3]), float(ele[4]), float(ele[5])*2e12/numMuonsTot, float(ele[6]), 
-                                           float(ele[7]), float(ele[8]), float(ele[9]), ele[10], float(ele[11]), float(ele[12]), 
-                                           float(ele[13]), int(ele[14]), ele[15], int(ele[16]) ))
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+                        fOut2.write("{:d}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\n"
+                                   .format(int(ele[4]), float(ele[5]), float(ele[6]), float(ele[7]),float(ele[8]), float(ele[9]), float(ele[10]),float(ele[11]), float(ele[12]),
+                                           float(ele[13]),float(ele[14]), float(nmd[7])*2e12/numMuonsTot))
+                    if line[0:2] == '  ':
+                        ele=line.split()
+                        fOut1.write("{:d}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\n"
+                                   .format(int(ele[0]), float(ele[1]), float(ele[2]), float(ele[3]), float(ele[4]), float(ele[5])*2e12/numMuonsTot, float(ele[6]),
+                                           float(ele[7]), float(ele[8]), float(ele[9]), float(ele[10]), float(ele[11]), float(ele[12]),
+                                           float(ele[13]), float(ele[14]), float(ele[15]), float(ele[16]), float(ele[17]), float(ele[18]), float(ele[19])))
+                    else:
+                        continue
