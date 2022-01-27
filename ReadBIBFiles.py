925bd4b5a19b681d72c6934d9ee8b424cd073a00
CamillaCuratolo <camilla.curatolo@pd.infn.it>
2022-01-27 08:49:37 +0100

    insert control on weight<1

--- ReadBIBFiles.py
+++ ReadBIBFiles.py
@@ -20,12 +20,12 @@ args = parser.parse_args()
 if args.dataPath:
     path=args.dataPath
 else:
-    path = "FLUKAresults/provanew" #/*"
+    path = "3TeV_base_pointa001_DUMP"
 
 if args.outPath:
     outfile=args.outPath
 else:
-    outfile = "FLUKAresults/3TeV_point_BH_WM_NEW"
+    outfile = "prova"
 
 
 numMuonsTot=0
@@ -55,12 +55,15 @@ with open(outfile,"w") as fOut1, open(outfile+'_ele',"w") as fOut2:
                 if  line.strip():
                     if line[1:5] == '## f':
                         ele=line.split()
+                        if float(ele[12])==float(ele[13])==float(ele[14])==0.00000000:
+                            ele[14]=-2000.
                         fOut2.write("{:d}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\n"
                                    .format(int(ele[4]), float(ele[5]), float(ele[6]), float(ele[7]),float(ele[8]), float(ele[9]), float(ele[10]),float(ele[11]), float(ele[12]),
                                            float(ele[13]),float(ele[14]), float(nmd[7])*2e12/numMuonsTot))
                     if line[0:2] == '  ':
                         ele=line.split()
-                        fOut1.write("{:d}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\n"
+                        if float(ele[5])<1.:
+                            fOut1.write("{:d}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\t{:e}\n"
                                    .format(int(ele[0]), float(ele[1]), float(ele[2]), float(ele[3]), float(ele[4]), float(ele[5])*2e12/numMuonsTot, float(ele[6]),
                                            float(ele[7]), float(ele[8]), float(ele[9]), float(ele[10]), float(ele[11]), float(ele[12]),
                                            float(ele[13]), float(ele[14]), float(ele[15]), float(ele[16]), float(ele[17]), float(ele[18]), float(ele[19])))
